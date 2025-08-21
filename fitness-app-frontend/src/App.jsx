98174786de2d7c2dbae3761ea02b5f0e81d56f15
Sujikanth <sujikanth77@gmail.com>
2025-08-21 22:23:21 +0530

    CROS error is there

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,11 +1,38 @@
 import { Button } from "@mui/material"
+import { useContext, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router"
+import { setCredentials } from "./store/authSlice";
+import { AuthContext } from "react-oauth2-code-pkce";
 
 function App() {
+  const { token, tokenData, login, logout, isAuthenticated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+  
+  // when token, tokenData, dispatch changes, it will be executed.
+  // if there is token, set the credentials and update in redux store
+  useEffect(() => {
+    if(token) {
+      dispatch(setCredentials({token, user: tokenData}));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch])
+
 
   return (
     <Router>
-      <Button variant="contained" color="#dc004e"> LOGIN </Button>
+      {!token ? ( // if there is no token, still show entire login
+        <Button variant="contained" color="#dc004e"
+            onClick={() => {
+              login();
+            }}
+        > LOGIN </Button>
+      ) : ( // if token is there, take token data and reder it
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+        </div>
+      )}
     </Router>
   )
 }
--- fitness-app-frontend/src/store/authSlice.js
+++ fitness-app-frontend/src/store/authSlice.js
@@ -9,10 +9,24 @@ const authSlice = createSlice({
   },
   reducers: {
     setCredentials: (state, action) => {
-      
+      //update the redux state and local storage when credentials are set
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.userId = action.payload.user.sub;
+
+      localStorage.setItem('token', action.payload.user);
+      localStorage.setItem('user', JSON.stringify(action.payload.user));  //user info is token decoded, it has to stringify
+      localStorage.setItem('userId', action.payload.user.sub);
     },
     logout: (state) => {
-      
+      //credentials will be reset when user logout
+      state.user = null;
+      state.token = null;
+      state.userId = null;
+
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      localStorage.removeItem('userId');
     },
   },
 })
